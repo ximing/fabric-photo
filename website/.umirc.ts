94ad4631ceea9adb256fc1e58f77fb559ee76f33
ximing <ximing@meituan.com>
2021-02-21 01:13:11 +0800

    docs: 添加demo

---
+++ website/.umirc.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+    title: 'fabric-photo',
+    favicon:
+        'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+    logo:
+        'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+    outputPath: 'dist',
+    base: '/fabric-photo',
+    publicPath:
+        process.env.NODE_ENV !== 'development' ? 'https://ximing.github.io/fabric-photo/' : '/'
+
+    // more config: https://d.umijs.org/config
+});
