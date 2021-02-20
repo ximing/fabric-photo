04365203df1a3b3a0115e28060b5ad208f484416
ximing <ximing@meituan.com>
2021-02-21 01:24:30 +0800

    feat: 修复doc

--- website/.umirc.ts
+++ website/.umirc.ts
@@ -7,9 +7,11 @@ export default defineConfig({
     logo:
         'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
     outputPath: 'dist',
+    hash: process.env.NODE_ENV !== 'development',
     base: '/fabric-photo',
     publicPath:
-        process.env.NODE_ENV !== 'development' ? 'https://ximing.github.io/fabric-photo/' : '/'
+        process.env.NODE_ENV !== 'development' ? 'https://ximing.github.io/fabric-photo/' : '/',
+    exportStatic: {}
 
     // more config: https://d.umijs.org/config
 });
