8c02a5156cc3d78f25d857a033ab28c395caa804
ximing <ximing@meituan.com>
2025-10-18 09:03:31 +0800

    feat: 重构Readme

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,7 @@
-/**
- * Created by yeanzhi on 17/1/13.
- */
 module.exports = {
-    plugins: [
-        require('postcss-flexbugs-fixes'),
-        require('postcss-preset-env')({
-            autoprefixer: {
-                flexbox: 'no-2009'
-            },
-            stage: 3
-        }),
-        require('postcss-clearfix')(),
-        require('postcss-position')(),
-        require('postcss-size')()
-    ]
+  plugins: {
+    'postcss-import': {},
+    tailwindcss: {},
+    autoprefixer: {}
+  }
 };
